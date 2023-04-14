fe61def24bbc18f0c575f82359966bed85dc3d77
Ingole <suraj.ingole@emerson.com>
2023-04-14 19:22:53 +0530

    challenge completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,22 +12,76 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
+// app.set('view/partial engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+//Global Variables
+  const postContent =[];
 
+//challenge one 
+app.get('/', (req, res) => {
+  res.render('home',{
+    home_para:homeStartingContent,
+    posts:postContent
+  });
+});
 
+app.get('/about', (req, res) => {
+  res.render('about',{
+    about_para:aboutContent
+  });
+});
 
+app.get('/contact', (req, res) => {
+  res.render('contact',{
+    contact_para:contactContent
+  });
+});
 
+app.get('/compose', (req, res) => {
+  res.render('compose',{
+  });
+});
 
+app.post('/compose', (req, res) => {
+  const postTitle = req.body.title;
+  const postBody = req.body.post;
 
+  if(postTitle != "" && postBody!=""){
+    postContent.push({
+      title:postTitle,
+      postBody:postBody
+    });
+  }
 
+  res.redirect("/");
+});
 
+app.get('/posts/:topic', (req, res) => {
+ for(var i = 0; i < postContent.length; i++){
 
+    if(_.lowerCase(postContent[i].title)===_.lowerCase(req.params.topic)){
+      res.render('post', {
+        title:postContent[i].title, 
+        postBody:postContent[i].postBody
+      });
+      break;
+    }
+  }
+  });
 
+function truncateString(str, maxLength) {
 
+      if(str.length <= maxLength) {
+        return str;
+      }
+    
+      const truncatedString = str.slice(0, maxLength);
+
+      return truncatedString + '...';
+}
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
